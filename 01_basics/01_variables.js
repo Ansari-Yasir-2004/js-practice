14ecef95b62b8e9663895233a37aedf19b690d9e
Ansari-Yasir-2004 <yasiransari8904@gmail.com>
2024-09-03 07:21:17 +0000

    #changes in basics

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 125548
+let accountEmail = "yasir123@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2134 // not allowed
+accountEmail = "yasir324@google,com"
+accountPassword = "65789"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
