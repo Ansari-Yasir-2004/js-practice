8a6b257895e7807f896076853d50f67a918b96f4
Ansari-Yasir-2004 <yasiransari8904@gmail.com>
2024-10-31 06:00:52 +0000

    strings summary

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,17 @@
+const name = "Yasir"
+const repoCount = 10
+
+// console.log(`My name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String("yasir-ff-com")
+
+// console.log(gameName.__proto__);
+
+
+let url = "https://yasir.com/yasir%20ansari"
+
+let newString = "console.log(url.replace('%20' , '_'))";
+
+console.log(newString);
+
+// console.log(newString.includes('yasir-ansari'));
