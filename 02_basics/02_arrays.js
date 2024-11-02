db888d657cb185603d423194eebf043d66d81fd0
Ansari-Yasir-2004 <yasiransari8904@gmail.com>
2024-11-02 07:37:29 +0000

    arrays 2 part 1

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,8 @@
+const marvelHeros = ["iron-man", "thor", "spider-man"]
+const dcHeros = ["bat-man", "flash", "super-man"]
+
+// marvelHeros.push(dcHeros)
+// console.log(marvelHeros[3][2]);
+
+const allHeros = marvelHeros.concat(dcHeros)
+console.log(allHeros);
