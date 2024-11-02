c15d4fe80a518633c25806dc026091c58a2f7eea
Ansari-Yasir-2004 <yasiransari8904@gmail.com>
2024-11-02 13:04:37 +0000

    arrays 2 part 2

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -4,5 +4,21 @@ const dcHeros = ["bat-man", "flash", "super-man"]
 // marvelHeros.push(dcHeros)
 // console.log(marvelHeros[3][2]);
 
-const allHeros = marvelHeros.concat(dcHeros)
-console.log(allHeros);
+// const allHeros = marvelHeros.concat(dcHeros)
+// console.log(allHeros);
+
+// const allNewHeros = [...marvelHeros, ...dcHeros]
+// console.log(allNewHeros);
+
+// const anotherArray = [1, 2, [3, 4], 5, 6, [7, [8, [9]]], 10]
+// const realAnotherArray = anotherArray.flat(3)
+// console.log(realAnotherArray);
+
+console.log(Array.isArray("yasir"));
+console.log(Array.from("yasir"))
+
+// const score_1 = 100
+// const score_2 = 200
+// const score_3 = 300
+
+// console.log(Array.of(score_1, score_2, score_3));
