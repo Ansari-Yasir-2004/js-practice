526a5e3715a439a32852ada76511bd31753e9dd0
Ansari-Yasir-2004 <yasiransari8904@gmail.com>
2024-11-03 03:45:56 +0000

    03 objects part 2

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -2,19 +2,33 @@
 // Object.create
 
 // object literals
-const mySym = Symbol(key1)
+const mySym = Symbol("key1")
 
 const jsUser = {
     name: "yasir",
     "full Name": "yasir ansari",
-    [mySym]: "key1",
+    [mySym]: "mykey1",
     age: 20,
     location: "maharashtra",
     email: "yasir@google.com",
     isLoggedIn: false,
-    lastLoginDays: ["monday", "friday"] 
+    lastLoginDays: ["monday", "friday"],
 }
-console.log(jsUser.email);
-console.log(jsUser["email"]);
-console.log(jsUser["full Name"]);
-console.log(jsUser[mySym]);
+// console.log(jsUser.email);
+// console.log(jsUser["email"]);
+// console.log(jsUser["full Name"]);
+// console.log(typeof jsUser[mySym]);
+
+jsUser.age = 21
+// Object.freeze(jsUser)
+jsUser.age = 22
+// console.log(jsUser);
+
+jsUser.greeting = function() {
+    console.log("hello Js user");
+}
+console.log(jsUser.greeting());
+jsUser.greetingTwo = function() {
+    console.log(`Hello Js User and ${this["full Name"]}`)
+}
+console.log(jsUser.greetingTwo());
