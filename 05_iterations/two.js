12098047f27abf02b88fd9bba287c6c500e86452
Ansari-Yasir-2004 <yasiransari8904@gmail.com>
2024-11-05 11:32:22 +0000

    two - while and doWhile loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+// while loop
+
+// let index = 1;
+// while (index <= 1000) {
+//     index *= index;
+//     console.log(`Value of index is ${index}`);
+//     index++
+// }
+
+// let myArray = ['flash', 'spider-man', 'bat-man'];
+// let arr = 0;
+// while (arr < myArray.length) {
+//     console.log(`value is ${myArray[arr]}`);
+//     arr = arr + 1;
+// }
+
+// do-while loop
+
+let score = 1;
+do {
+    console.log(`value of score is ${score}`);
+    score++;
+} while (score <= 10)
